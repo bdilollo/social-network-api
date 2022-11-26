55bd117f53f7a578a650e61be1be496e75e051be
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-11-26 17:17:31 -0600

    created basic routes, no controllers

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+// const { } = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+module.exports = router;
