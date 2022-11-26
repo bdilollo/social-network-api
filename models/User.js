12309ee1dedc8e2228faeee7e4d22fb1fbbc8d78
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-11-26 13:26:07 -0600

    created schema for each model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+        },
+        thoughts: {
+
+        },
+        friends: {
+
+        }
+    }
+)
+
+module.exports = User;
